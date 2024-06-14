7d053a3892eb097b6d452a0ddebe8b1c7241063e
r_cazoulat <61828714+RemiCazoulat@users.noreply.github.com>
2024-06-14 17:12:51 +0200

    test

--- UnityNucFu/Assets/Scripts/Particles/Particle.cs
+++ UnityNucFu/Assets/Scripts/Particles/Particle.cs
@@ -4,21 +4,43 @@ using UnityEngine;
 
 public struct Particle
 {
-    public Vector3 position;
-    public Vector3 direction;
+    public Vector3 pos;
+    public Vector3 dir;
+    public Vector3 force;
     public float charge;
     public float mass;
 
     public Particle(Vector3 p, Vector3 d, float c, float m)
     {
-        position = p;
-        direction = d;
+        pos = p;
+        dir = d;
+        force = Vector3.zero;
         charge = c;
         mass = m;
     }
 
+    public void ResetForce()
+    {
+        force = Vector3.zero;
+    }
+
+    public void AddForce(Vector3 newForce)
+    {
+        force += newForce;
+    }
+
+    public void ModifyPos(Vector3 newPos)
+    {
+        pos = newPos;
+    }
+
+    public void ModifyDir(Vector3 newDir)
+    {
+        dir = newDir;
+    }
+
     public static  int GetSize()
     {
-        return 12 + 12 + 4 + 4;
+        return 12 + 12 + 12 + 4 + 4;
     }
 }
