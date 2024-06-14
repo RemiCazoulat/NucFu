9ed759a4613d6304d0fe13eca536e10fc6f38a44
r_cazoulat <61828714+RemiCazoulat@users.noreply.github.com>
2024-06-14 11:23:12 +0200

    add GlNucFu

---
+++ GLNucFu/cmake-build-debug/CMakeFiles/GLNucFu.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GLNucFu.dir/main.cpp.obj"
+  "CMakeFiles/GLNucFu.dir/main.cpp.obj.d"
+  "GLNucFu.exe"
+  "GLNucFu.exe.manifest"
+  "GLNucFu.pdb"
+  "libGLNucFu.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GLNucFu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
